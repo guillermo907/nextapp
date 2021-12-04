b97a1d45e1179270e4e8b7771e856cafb5e65494
Guillermo Lòpez Garcìa <willy@MacBook-Pro-de-Guillermo.local>
2021-12-03 22:14:54 -0600

    changes

--- components/Signcard.js
+++ components/Signcard.js
@@ -7,14 +7,26 @@ const CardContainer = styled.div`
   min-height: 60%;
   min-width: 70%;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  button {
+    border-radius: 12px;
+    padding: 10px 22px;
+    margin: 12px;
+    background-color: var(--color-one);
+    color: white;
+    border: none;
+    cursor: pointer;
+  }
 `;
 
 const Signcard = () => {
   return (
     <CardContainer className="box-shadow glass">
-      <h1>Banner</h1>
+      <h1>Crea tu cuenta</h1>
+      <button className="box-shadow">Suscribirse</button>
     </CardContainer>
   );
 };
